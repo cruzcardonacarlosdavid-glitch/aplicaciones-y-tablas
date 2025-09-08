ebf085629c915ba22a0275d43bb5553672312579
carlos4090k <estudiantev@COED.LOCAL>
2025-09-08 15:10:24 -0600

    se creo la app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from sqlite3 import *
+from tkinter import *
+from tkinter import ttk
 
 base_de_datos = connect("tabla.db")
 cr = base_de_datos.cursor()
@@ -14,3 +16,43 @@ def crearTabla():
     base_de_datos.commit()
     print("Creado")
 
+cr.execute('''SELECT * FROM estudiantes;''')
+datos =cr.fetchall()
+
+def rellenar_tabla(tabla):
+    for dato in datos:
+        tabla.insert("", "end", values=dato)
+
+#Iniciamos la aplicacion
+app = Tk()
+app.title("Aplicacion y tablas")
+
+#Creamos el widget Treeview
+tabla = ttk.Treeview(app, columns=("ID", "Nombre", "Edad","Stock"), show="headings")
+
+#Colocamos sus encabezados
+tabla.heading("ID", text="ID")
+tabla.heading("Nombre", text="Nombre")
+tabla.heading("Edad", text="Edad")
+tabla.heading("Stock", text ="Stock")
+
+#Ajustar el ancho de las columnas
+for col in tabla["columns"]:
+    tabla.column(col, anchor="center", width=100)
+
+
+
+#Empaquetar el widget Treeview
+tabla.pack()
+
+
+
+
+#Se invocan los métodos para ejemplificar
+rellenar_tabla(tabla)
+
+
+
+
+#Se inicia el bucle principal
+app.mainloop()
